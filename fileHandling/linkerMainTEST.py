d18405798a5f4c1736977227c2bec28f84e61dee
JohannesJamroszczyk <69056273+JohannesJamroszczyk@users.noreply.github.com>
2021-02-25 17:58:51 +0100

    test

--- fileHandling/linkerMainTEST.py
+++ fileHandling/linkerMainTEST.py
@@ -1,16 +1,14 @@
 from linkerFileCreation import *
 from formattingTools import *
-import pandas as pd
-
 
 label_file = mk_text_file("Label Files/label1.lbl")
 config_file = mk_text_file("Label Files/config.cfg")
 
 df = csv_to_matrix("testData.csv")
 
-print(df)
+configMatrix = popped_matrix(df, 0)
 
 #lable_file.write("This is a labelFile")
-config_file.write(matrix_to_config(df))
+config_file.write(matrix_to_config(configMatrix))
 
 
